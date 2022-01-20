6c67bacd8b6d44b8488f499a1d7a4ffd9bd5b537
Namit Mohan Barman <b.namit87@gmail.com>
2022-01-20 12:26:36 -0500

    Updates

--- src/test/scala/simulations/BasicLoadSimulation.scala
+++ src/test/scala/simulations/BasicLoadSimulation.scala
@@ -24,54 +24,78 @@ class BasicLoadSimulation extends Simulation {
   }
 
   def getSpecificGame(): ChainBuilder ={
-    exec(
-
-      http("get specific game")
-        .get("videogames/2")
-        .check(status.not(404))
-        .check(status.in(200 to 210)) //Status Range
-    )
+    repeat(2){ //Looping two times
+        exec(
+          http("get specific game")
+            .get("videogames/2")
+            .check(status.not(404))
+            .check(status.in(200 to 210)) //Status Range
+        )
+    }
   }
+
 
   //PAUSE Scenario and Scenario 1
   val scn1 = scenario("Basic Load simulation")
     .exec(getAllVideoGames()) //Code Reuse
     .pause(5000.milliseconds)
-    .exec(getSpecificGame())
+    .exec{session => println(session);session} // Print session vars for debugging
 
-  //JSONPATH and Scenario 2
-  val scn2 = scenario("Gatling Load Testing")
+  val scn2 = scenario("Loop HTTP calls")
+            .exec(getSpecificGame())
+
+  //JSONPATH and others
+  val scn3 = scenario("Gatling Load Testing")
 
     .exec(http("Get particular gameID").
       get("videogames")
       .check(jsonPath("$[1].id").//JSONPATH Definition
-        saveAs("gameId")))
-
-    .exec(http("Get particular gameID").
-      get("videogames"). //ENDPOINT
-      check(jsonPath("$[1].id").
-        saveAs("gameId")))
+        saveAs("gameId"))) //SaveAs Param for later use
 
     .exec(http("Get the game from stored param")
-      .get("videogames/${gameId}")
+      .get("videogames/${gameId}") //Using from the SaveAs
       .check(jsonPath("$.name")
         .is("Grand Turismo 3"))
       .check(bodyString.saveAs("response")))
-
     .exec{session => println(session("response").as[String]); session}
 
-    .exec{session => println(session); session}
+    //Feeder
+    val csvFeeder = csv("data/gameCsvFile.csv").circular
+    def getSpecificGameFromFeeder(): ChainBuilder ={
+      repeat(4){
+        feed(csvFeeder)
+          .exec(http("Get specific video game from Feeder")
+          .get("videogames/${gameID}")
+            .check(jsonPath("$.name").is("${gameName}"))
+          ).pause(1.second)
+      }
+    }
 
+    val feederScn = scenario("CSV Feeder Scenario")
+    .exec(getSpecificGameFromFeeder())
 
   //Load Scenarios
   setUp(
-    scn1.inject(
-      nothingFor(5 seconds),
-      atOnceUsers(5),
-      // rampUsers(10) during(10) // 10 users for 10 secs
-      rampUsersPerSec(1) to(10) during(20 seconds)
-    ).protocols(httpConf.inferHtmlResources())
-  ).maxDuration(1 minute) //Specifying a fixed duration of time
+                new BasicLoadSimulation().scn1.inject(
+                  nothingFor(5 seconds),
+                  atOnceUsers(5)
+                ).protocols(httpConf.inferHtmlResources()),
+
+                new BasicLoadSimulation().scn2.inject(
+                  nothingFor(5 seconds),
+                  // rampUsers(10) during(10) // 10 users for 10 secs
+                  rampUsersPerSec(1) to(10) during(20 seconds)
+                ).protocols(httpConf.inferHtmlResources()),
+
+                new BasicLoadSimulation().scn3.inject(
+                  nothingFor(5 seconds),
+                  // rampUsers(10) during(10) // 10 users for 10 secs
+                  rampUsersPerSec(1) to(10) during(20 seconds)
+                ).protocols(httpConf.inferHtmlResources())
+
+      ).maxDuration(2 minute) //Specifying a fixed duration of time
+
+
 
 
 }
